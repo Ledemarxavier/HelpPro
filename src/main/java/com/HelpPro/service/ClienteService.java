e1705c3a4df31e4e9afde11d473d83fbd879466a
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-13 20:55:44 -0300

    Criar classe UsuarioService com método CRUD

--- src/main/java/com/HelpPro/service/ClienteService.java
+++ src/main/java/com/HelpPro/service/ClienteService.java
@@ -1,12 +1,12 @@
 package com.HelpPro.service;
 
-import java.util.List;
+import com.HelpPro.model.Cliente;
+import com.HelpPro.repository.ClienteRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.HelpPro.model.Cliente;
-import com.HelpPro.repository.ClienteRepository;
+import java.util.List;
 
 @Service
 public class ClienteService {
---
+++ src/main/java/com/HelpPro/service/UsuarioService.java
@@ -0,0 +1,34 @@
+package com.HelpPro.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.HelpPro.model.Usuario;
+import com.HelpPro.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> getAllUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario saveUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deleteUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+    public Optional<Usuario> findByEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
+    public boolean existsByEmail(String email) {
+        return usuarioRepository.existsByEmail(email);
+    }
+}
