a608a52503b048b5bb995c9f749d79ba299bc27a
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-13 12:25:21 -0300

    Criar interface ProfissionalRepository com JpaRepository

---
+++ src/main/java/com/HelpPro/repository/ProfissionalRepository.java
@@ -0,0 +1,12 @@
+package com.HelpPro.repository;
+
+import com.HelpPro.model.Profissional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ProfissionalRepository extends JpaRepository<Profissional, Long>{
+    
+}
