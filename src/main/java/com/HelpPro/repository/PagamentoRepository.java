8f19e9fcf9748000998cf7a839240b59fa2aa776
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-13 12:46:51 -0300

    Criar interface PagamentoRepository com JpaRepository

---
+++ src/main/java/com/HelpPro/repository/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.HelpPro.repository;
+
+import com.HelpPro.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {   
+}
