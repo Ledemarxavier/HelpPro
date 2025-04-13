b54805ef6e7c673f8a66d1b29acf5d8b5c0c37ac
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-13 13:55:53 -0300

    Criar classe ClienteService com métodos CRUD

---
+++ src/main/java/com/HelpPro/service/ClienteService.java
@@ -0,0 +1,27 @@
+package com.HelpPro.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.HelpPro.model.Cliente;
+import com.HelpPro.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+    public Cliente saveCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void deleteCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
